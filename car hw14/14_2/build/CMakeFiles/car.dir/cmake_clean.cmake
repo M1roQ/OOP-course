0004de3f8c33674c63af9b63a90193be14c556c5
M1roQ <b_val4@mail.ru>
2025-04-01 12:12:05 +0700

    Added task14 from homework

---
+++ car hw14/14_2/build/CMakeFiles/car.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car.dir/car14_2.cpp.o"
+  "CMakeFiles/car.dir/car14_2.cpp.o.d"
+  "car"
+  "car.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/car.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
